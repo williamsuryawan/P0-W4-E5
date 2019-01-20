18cee27ffc0c3f4b9d64acecee8c5eb66e1ebe25
William Suryawan <suryawan.william@gmail.com>
2019-01-20 18:41:15 +0700

    tambah file

---
+++ William-P0-W4-E5.js
@@ -0,0 +1,24 @@
+function ubahHuruf(kata) {
+  // you can only write your code here!
+  var abjad = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var locate = [];
+  var baru = "";
+  for(i=0; i<kata.length;i++){
+    for(j=0; j<abjad.length; j++){
+      if(kata[i] == abjad[j]){
+        locate.push(j);
+      }
+    }
+  }
+  for(k=0; k<locate.length; k++){
+    baru += abjad[locate[k]+1];
+  }
+  return baru;
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
